ba61b8c81f3ef8359d83bb70fe8361f7575b4abc
zmerino <zmerino2718@gmail.com>
2025-05-09 17:57:56 -0400

    pushing NMEM smooth estimator cpp code to remote

--- functions_plotting/fig_export.m
+++
@@ -1,9 +0,0 @@
-function fig_export(handle,fig_name,fig_style)
-
-
-style = hgexport('readstyle',fig_style);
-style.Format = 'png';
-hgexport(handle,fig_name,style);
-style.Format = 'fig';
-hgexport(handle,fig_name,style);
-end
